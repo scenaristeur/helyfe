d7d7c7c38bc73a2a9b0d0b1d2b88520561a631f1
scenaristeur <scenaristeur@gmail.com>
2023-05-29 12:48:08 +0200

    dat.gui controls

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ global.THREE = require("three");
 require("three/examples/js/controls/OrbitControls");
 require("three/examples/js/geometries/ParametricGeometry.js");
 
-const dat = require('dat.gui');
+const dat = require("dat.gui");
 
 const canvasSketch = require("canvas-sketch");
 const { MeshPhysicalMaterial } = require("three");
@@ -23,10 +23,7 @@ const sketch = ({ context }) => {
     canvas: context.canvas,
   });
 
-  let params = {torsion: 5}; // torsion
-
- 
-
+  let params = { torsion: 5 }; // torsion
 
   // WebGL background color
   renderer.setClearColor("#000", 1);
@@ -48,7 +45,7 @@ const sketch = ({ context }) => {
   function Helicoid(u, v, target) {
     let alpha = Math.PI * 2 * u;
     let theta = Math.PI * 2 * v;
-   
+
     // sphere
     // let x = Math.sin(alpha)*Math.cos(theta)
     // let y = Math.sin(alpha)*Math.sin(theta)
@@ -67,9 +64,9 @@ const sketch = ({ context }) => {
     // hyperbola
     let bottom = 1 + Math.cosh(alpha) * Math.cosh(theta);
     // selon wolfram
-    let x = Math.sinh(theta) * Math.cos(params.torsion * alpha)/bottom
-    let y = Math.sinh(theta) * Math.sin(params.torsion * alpha)/bottom
-    let z = Math.cosh(theta) * Math.sinh( alpha)/bottom
+    let x = (Math.sinh(theta) * Math.cos(params.torsion * alpha)) / bottom;
+    let y = (Math.sinh(theta) * Math.sin(params.torsion * alpha)) / bottom;
+    let z = (Math.cosh(theta) * Math.sinh(alpha)) / bottom;
 
     // selon video à 24 mins
     // let x = Math.sinh(alpha) * Math.cos(t * theta)/bottom
@@ -100,8 +97,6 @@ const sketch = ({ context }) => {
     //fog: true,
     //wireframe: true
   });
-  
- 
 
   // Setup a mesh with geometry + material
   const helicoidMesh = new THREE.Mesh(geometry, material);
@@ -118,31 +113,42 @@ const sketch = ({ context }) => {
 
   const materialParams = {
     helicoidMeshColor: helicoidMesh.material.color.getHex(),
-    helicoidMeshEmissive: helicoidMesh.material.emissive.getHex()
-  }
+    helicoidMeshEmissive: helicoidMesh.material.emissive.getHex(),
+  };
   //dat.GUI
-  const gui = new dat.GUI()
-  const helicoidFolder = gui.addFolder('Helicoid')
+  const gui = new dat.GUI();
+  const helicoidFolder = gui.addFolder("Helicoid");
+  //helicoidFolder.open();
   // https://www.youtube.com/watch?v=JyhhHhoqK2o&t=224s
-  helicoidFolder.add(helicoidMesh.rotation, 'x', 0, Math.PI).name('Rotate X')
-  helicoidFolder.add(helicoidMesh.rotation, 'y', 0, Math.PI).name('Rotate Y')
-  helicoidFolder.add(helicoidMesh.rotation, 'z', 0, Math.PI).name('Rotate z')
-  helicoidFolder.add(helicoidMesh.scale, 'x',0, 2).name('Scale x')
-  helicoidFolder.add(helicoidMesh.scale, 'y',0, 2).name('scale y')
-  helicoidFolder.add(helicoidMesh.scale, 'z',0, 2).name('scale z')
-  //color & emissive color
-  helicoidFolder.add(helicoidMesh.material, 'wireframe')
-  helicoidFolder.addColor(materialParams, 'helicoidMeshColor').name('color').onChange((value) => helicoidMesh.material.color.set(value))
-  helicoidFolder.addColor(materialParams, 'helicoidMeshEmissive').name('emissive color').onChange((value) => helicoidMesh.material.emissive.set(value))
+  const geometryFolder = helicoidFolder.addFolder("Geometry");
+  geometryFolder.add(helicoidMesh.rotation, "x", 0, Math.PI).name("Rotate X");
+  geometryFolder.add(helicoidMesh.rotation, "y", 0, Math.PI).name("Rotate Y");
+  geometryFolder.add(helicoidMesh.rotation, "z", 0, Math.PI).name("Rotate z");
+  //geometryFolder.open()
 
-  helicoidFolder.add(params, 'torsion', 0,5).name('Set torsion')
-  // helicoidFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-  // helicoidFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-  helicoidFolder.open()
+  const scaleFolder = helicoidFolder.addFolder("scale");
+  scaleFolder.add(helicoidMesh.scale, "x", 0, 2).name("Scale x");
+  scaleFolder.add(helicoidMesh.scale, "y", 0, 2).name("scale y");
+  scaleFolder.add(helicoidMesh.scale, "z", 0, 2).name("scale z");
+  scaleFolder.open();
+  //color & emissive color
+  const materialFolder = helicoidFolder.addFolder("Material");
+  materialFolder.add(helicoidMesh.material, "wireframe");
+  materialFolder
+    .addColor(materialParams, "helicoidMeshColor")
+    .name("color")
+    .onChange((value) => helicoidMesh.material.color.set(value));
+  materialFolder
+    .addColor(materialParams, "helicoidMeshEmissive")
+    .name("emissive color")
+    .onChange((value) => helicoidMesh.material.emissive.set(value));
+  materialFolder.open();
+
+  const todoFolder = helicoidFolder.addFolder("todo");
+  todoFolder.add(params, "torsion", 0, 5).name("torsion(todo)");
   // const cameraFolder = gui.addFolder('Camera')
   // cameraFolder.add(camera.position, 'z', 0, 10)
   // cameraFolder.open()
-
 
   // draw each frame
   return {
@@ -167,5 +173,3 @@ const sketch = ({ context }) => {
 };
 
 canvasSketch(sketch, settings);
-
-

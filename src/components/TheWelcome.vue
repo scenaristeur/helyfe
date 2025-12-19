ffeaee98c293906c3816eb524d8c86b0abe22e48
scenaristeur <scenaristeur@gmail.com>
2025-12-19 18:45:19 +0100

    plane & sphere

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -6,65 +6,91 @@ import ForceGraph3D from '3d-force-graph';
 import * as THREE from 'three';
 export default {
     name: "WelcomeView",
+    // data() {
+    //     // return {
+    //     //     graph: null
+    //     // }
+    // },
     mounted() {
         // this.$graphInit({ domElement: this.$refs.graph })
-        const N = 300;
-        const gData = {
-            nodes: [...Array(N).keys()].map(i => ({ id: i })),
-            links: [...Array(N).keys()]
-                .filter(id => id)
-                .map(id => ({
-                    source: id,
-                    target: Math.round(Math.random() * (id - 1))
-                }))
-        };
-
-        const graph = new ForceGraph3D(document.getElementById('3d-graph'))
-            .graphData(gData);
-
-        const planeGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
-        const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xFF0000, side: THREE.DoubleSide });
-        const mesh = new THREE.Mesh(planeGeometry, planeMaterial);
-        mesh.position.set(-100, -200, -100);
-        mesh.rotation.set(0.5 * Math.PI, 0, 0);
 
 
 
-        // const Graph = new ForceGraph3D(document.getElementById('graph'))
-        //     .graphData(gData);
-        graph.scene().add(mesh);
+
+        this.initGraph()
+        this.addPlane()
+        this.addSphere()
     },
     methods: {
-        update() {
-            if (this.graph != undefined) {
-                let nodes = this.nodes.map(a => { return { ...a } })
-                let links = this.links.map(a => { return { ...a } })
-                this.graph.graphData({ nodes: nodes, links: links })
-                console.log(this.graph.graphData())
-            }
+        initGraph() {
+            const N = 300;
+            const gData = {
+                nodes: [...Array(N).keys()].map(i => ({ id: i })),
+                links: [...Array(N).keys()]
+                    .filter(id => id)
+                    .map(id => ({
+                        source: id,
+                        target: Math.round(Math.random() * (id - 1))
+                    }))
+            };
+
+            this.graph = new ForceGraph3D(document.getElementById('3d-graph'))
+                .graphData(gData);
+        },
+        addPlane() {
+            const planeGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
+            const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xFF0000, side: THREE.DoubleSide });
+            const mesh = new THREE.Mesh(planeGeometry, planeMaterial);
+            mesh.position.set(-100, -200, -100);
+            mesh.rotation.set(0.5 * Math.PI, 0, 0);
+            this.graph.scene().add(mesh);
+        },
+        addSphere() {
+            const sphereGeometry = new THREE.SphereGeometry(100, 32, 16)
+            const sphereMaterial = new THREE.MeshBasicMaterial({
+                color: "blue",
+                wireframe: true
+            })
+
+            const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+            sphereMesh.position.set(-500, 400, -100);
+
+
+            // const Graph = new ForceGraph3D(document.getElementById('graph'))
+            //     .graphData(gData);
+
+            this.graph.scene().add(sphereMesh)
         }
+        // update() {
+        //     if (this.graph != undefined) {
+        //         let nodes = this.nodes.map(a => { return { ...a } })
+        //         let links = this.links.map(a => { return { ...a } })
+        //         this.graph.graphData({ nodes: nodes, links: links })
+        //         console.log(this.graph.graphData())
+        //     }
+        // }
     },
     watch: {
-        nodes() {
-            this.update()
-        },
-        links() {
-            this.update()
-        },
-        graph() {
-            this.update()
-        }
+        // nodes() {
+        //     this.update()
+        // },
+        // links() {
+        //     this.update()
+        // },
+        // graph() {
+        //     this.update()
+        // }
     },
     computed: {
-        nodes() {
-            return this.$store.state.core.nodes
-        },
-        links() {
-            return this.$store.state.core.links
-        },
-        graph() {
-            return this.$store.state.core.graph
-        }
+        // nodes() {
+        //     return this.$store.state.core.nodes
+        // },
+        // links() {
+        //     return this.$store.state.core.links
+        // },
+        // graph() {
+        //     return this.$store.state.core.graph
+        // }
     },
 }
 </script>

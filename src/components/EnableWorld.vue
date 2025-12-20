694a0b1bfc6a47c58bd273eb0804e7c5d0854018
scenaristeur <scenaristeur@gmail.com>
2025-12-20 10:06:13 +0100

    add enable3D

--- src/components/EnableWorld.vue
+++ src/components/EnableWorld.vue
@@ -1,19 +1,59 @@
 <template>
-    <div>EnableWorld</div>
+    <div id="enable3d-container"></div>
 </template>
 <script>
+// import the UMD bundle enable3d.framework.min.js
+// or from npm enable3d
+import { Project, Scene3D, PhysicsLoader } from 'enable3d'
 
-export default {
-    name: "EnableView",
-    // data() {
-    //     // return {
-    //     //     graph: null
-    //     // }
-    // },
-    mounted() {
-
-
+class MainScene extends Scene3D {
+    constructor() {
+        super('MainScene')
     }
 
+    async init() {
+        this.renderer.setPixelRatio(1)
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+
+    async preload() {
+        // preload your assets here
+    }
+
+    async create() {
+        // set up scene (light, ground, grid, sky, orbitControls)
+        this.warpSpeed()
+
+        // enable physics debug
+        this.physics.debug.enable()
+
+        // position camera
+        this.camera.position.set(10, 10, 20)
+
+        // blue box (without physics)
+        this.add.box({ y: 2 }, { lambert: { color: 'deepskyblue' } })
+
+        // pink box (with physics)
+        this.physics.add.box({ y: 10 }, { lambert: { color: 'hotpink' } })
+    }
+
+    update() {
+        // this.box.rotation.x += 0.01
+        // this.box.rotation.y += 0.01
+    }
+}
+
+// set your project configs
+const config = { scenes: [MainScene] }
+
+// load the ammo.js file from the /lib folder and start the project
+PhysicsLoader('/lib', () => new Project(config))
+
+export default {
+    name: "EnableWorld",
+    mounted() {
+        // Initialisation de la scène 3D
+        // Le code ci-dessus gère déjà l'initialisation
+    }
 }
 </script>
